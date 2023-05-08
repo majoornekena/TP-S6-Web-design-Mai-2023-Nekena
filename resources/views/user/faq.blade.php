fbd0ac15f279842ea5738b98447b95cea2533e6c
Hasinjaka)# <andrianomehasinjaka@gmail.com>
2023-05-08 18:45:44 +0300

    ok

--- resources/views/user/faq.blade.php
+++ resources/views/user/faq.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta name="description" content="E-Art ETU001670 le site de Hardi qui concerne l IA">
-  <title>E-Art - ETU001670</title>
+  <title>ARTISTA-ETU001664/title>
   <meta content="" name="keywords">
 
   <!-- Favicons -->
