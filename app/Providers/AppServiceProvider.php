9a5983a91ca28377d50997f04d259506afcadff5
Hasinjaka)# <andrianomehasinjaka@gmail.com>
2023-05-08 21:14:01 +0300

    ok

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // //
-        // if (request()->server('HTTP_ACCEPT_ENCODING') && strpos(request()->server('HTTP_ACCEPT_ENCODING'), 'gzip') !== false) {
-        //     ob_start("ob_gzhandler");
-        // }        
+        //
+        if (request()->server('HTTP_ACCEPT_ENCODING') && strpos(request()->server('HTTP_ACCEPT_ENCODING'), 'gzip') !== false) {
+            ob_start("ob_gzhandler");
+        }        
     }
 }
